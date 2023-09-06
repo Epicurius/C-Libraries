285be4cf2ea84eae7f20f23e368c7457afb70cb3
Epicurius <niklas0702@gmail.com>
2023-09-06 13:09:16 +0300

    libif: fix custom color codes
    This patch fixes color code issue from commit:
    dbe7c8d libpf: fix color codes
    
    Signed-off-by: Epicurius <niklas0702@gmail.com>

--- libpf/srcs/pf_specials.c
+++ libpf/srcs/pf_specials.c
@@ -23,34 +23,35 @@ static const t_pf_style	g_pf_styles[11] =
 	{"{INVERT}", "\x1b[07m", 8}
 };
 
-static int	color_code(t_pf *p, int i)
+static void	color_code(t_pf *p)
 {
 	int	len;
 	int	code;
 
 	len = 0;
-	code = ft_atoi(p->format + i);
+	code = ft_atoi(p->format + 5);
 	if (code >= 0 && code <= 9)
 		len = 1;
 	else if (code >= 10 && code <= 99)
 		len = 2;
 	else if (code >= 100 && code <= 255)
 		len = 3;
-	if (len == 0 || !ft_strncmp((p->format + i + len), "}", 1))
-		return 0;
+	if (len == 0 || ft_strncmp((p->format + 5 + len), "}", 1)) {
+		fill_buffer(p, p->format, 1);
+		return ;
+	}
 	if (fileno(p->fp) < 3) {
 		fill_buffer(p, "\x1b[38;5;", 7);
-		fill_buffer(p, p->format + i, len);
+		fill_buffer(p, p->format + 5, len);
 		fill_buffer(p, "m", 1);
 	}
-	p->format += i + len + 1 - 1;
-	return 1;
+	p->format += 5 + len;
 }
 
 static void	color_preset(t_pf *p)
 {
 	for (int i = 0; i < 11; i++) {
-		if (!ft_strncmp(g_pf_styles[i].str, &*p->format, g_pf_styles[i].len)) {
+		if (!ft_strncmp(g_pf_styles[i].str, p->format, g_pf_styles[i].len)) {
 			if (fileno(p->fp) < 3)
 				fill_buffer(p, g_pf_styles[i].code, 5);
 			p->format += g_pf_styles[i].len - 1;
@@ -63,8 +64,8 @@ static void	color_preset(t_pf *p)
 void	pf_specials(t_pf *p)
 {
 	if (*p->format == '{' && ft_isupper(*(p->format + 1))) {
-		if (!ft_strncmp(&*p->format, "{CLR:", 5))
-			color_code(p, 5);
+		if (!ft_strncmp(p->format, "{CLR:", 5))
+			color_code(p);
 		else
 			color_preset(p);
 	}
